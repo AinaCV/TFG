be184e9ff0c1ab1fdab3f6b778ccc3c77ae61efd
ainaUwU <ainacrespiv@gmail.com>
2023-06-10 04:03:26 +0200

    Falta yRotation

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Player player;
+    public Transform cameraPivot;
+
     public float mouseSens = 400f;
-    private float yRotation = 0f;
+    public float smoothTime = 0.2f;
     public float maxY;
     public float minY;
-    //public GameObject followTransform;
+    public float rotationSpeed = 5f;
+    public bool isMoving = false;
+    public Transform target;
+    private float yRotation = 0f;
+    private float xRotation = 0f;
+    private Vector3 lastCameraPosition;
+    private Vector3 currentVelocity;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked; // para que desaparezca el cursor
+        lastCameraPosition = cameraPivot.position;
     }
 
     void Update()
@@ -25,27 +34,70 @@ public class CameraController : MonoBehaviour
         }
         else
         {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if (player.isWalking || player.isRunning)
+
+        {
+
             float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
 
             yRotation -= mouseY; // el menos es importante
-            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(yRotation, 0, 0), 1f); //Lerp = interpolacion, euler es un vector normal
-                                                                                                                       //var angles = followTransform.transform.localEulerAngles;
-                                                                                                                       //transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
-                                                                                                                       //followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(yRotation, 0, 0), 1f);
 
             player.transform.Rotate(Vector3.up * mouseX); //movimiento horizontal 
-
-            if (yRotation >= maxY)
-            {
-                yRotation = maxY;
-            }
-            if (yRotation <= minY)
-            {
-                yRotation = minY;
-            }
-            Cursor.lockState = CursorLockMode.Locked;
+            isMoving = true;
         }
+        else
+        {
+            isMoving = false;
+            float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+            yRotation -= mouseY; // el menos es importante
+            xRotation += mouseX; // el menos es importante
+
+            yRotation = Mathf.Clamp(yRotation, minY, maxY); // límites para yRotation
+            //Quaternion camTargetRotation = Quaternion.Euler(yRotation, xRotation, 0); // Usar ambas rotaciones
+            //transform.localRotation = Quaternion.Lerp(transform.localRotation, camTargetRotation, 1f);
+            cameraPivot.RotateAround(player.transform.position, Vector3.up, mouseX * rotationSpeed);
+            cameraPivot.rotation = Quaternion.Euler(yRotation, cameraPivot.rotation.eulerAngles.y, 0);
+        }
+
+        if (player.isWalking || player.isRunning)
+        {
+            transform.LookAt(player.transform);
+            isMoving = true;
+        }
+
+        if (!player.isWalking || !player.isRunning)
+        {
+            transform.LookAt(target.transform);
+            isMoving = false;
+            //AudioManager.Instance.PlayAudio(clip.name);
+        }
+
+        ChangeBoolState();
+
+        if (yRotation >= maxY)
+        {
+            yRotation = maxY;
+        }
+        if (yRotation <= minY)
+        {
+            yRotation = minY;
+        }
+    }
+    void ChangeBoolState()
+    {
+        isMoving = !isMoving; //comprueba en que estado se encuentra el bool y lo cambia al pulsar la tecla
+
+        // Iniciar el movimiento suave de la cámara hacia la última posición cuando el jugador deja de moverse
+        cameraPivot.position = Vector3.SmoothDamp(cameraPivot.position, lastCameraPosition, ref currentVelocity, smoothTime);
+        // Actualizar la última posición de la cámara cuando el jugador comienza a moverse
+        lastCameraPosition = cameraPivot.position;
     }
 
     private void OnDestroy()
