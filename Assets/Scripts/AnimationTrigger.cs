e7522b07431f7af2b5388f0697d6c1418f4d0d60
ainaUwU <ainacrespiv@gmail.com>
2023-06-11 04:50:21 +0200

    SceneManager

---
+++ Assets/Scripts/AnimationTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+    Animator anim;
+    public GameObject gideon;
+
+    private void Start()
+    {
+        anim = gideon.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player != null)
+        {
+            anim.Play("Lift");
+            GameManager.instance.EndAnimation(3);
+        }
+    }
+}
